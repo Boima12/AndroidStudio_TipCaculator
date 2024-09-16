882ec366fc1a3738b784f516fe263a0b30f23646
Boima <Boimache27@gmail.com>
2024-09-16 13:51:00 +0700

    first commit

---
+++ app/src/androidTest/java/com/example/tipcaculator2/ExampleInstrumentedTest.kt
@@ -0,0 +1,26 @@
+package com.example.tipcaculator2
+
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.ext.junit.runners.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+import org.junit.Rule
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+
+    @Test
+    fun calculate_20_percent_tip() {
+
+    }
+
+}
---
+++ app/src/test/java/com/example/tipcaculator2/ExampleUnitTest.kt
@@ -0,0 +1,24 @@
+package com.example.tipcaculator2
+
+import android.icu.text.NumberFormat
+import org.junit.Test
+
+import org.junit.Assert.*
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class ExampleUnitTest {
+    @Test
+    fun calculateTip_20PercentNoRoundup() {
+        val amount = 10.00
+        val tipPercent = 20.00
+        val expectedTip = NumberFormat.getCurrencyInstance().format(2)
+        val actualTip = calculateTip(amount = amount, tipPercent = tipPercent, false)
+
+
+        assertEquals(expectedTip, actualTip)
+    }
+}
